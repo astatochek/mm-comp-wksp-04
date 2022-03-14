061d63d9aef7b8981badb154c58714ce5399ab20
astatochek <71458498+astatochek@users.noreply.github.com>
2022-03-14 23:06:10 +0300

    upd

--- 3.  Inversions/functions.py
+++ 3.  Inversions/functions.py
@@ -23,10 +23,10 @@ def promptPair() -> list:
 
 
 def promptM() -> int:
-    print(f">>    Insert values for m")
+    print(f">>    Insert value for m")
     inpt = input()
     try:
-        m = int(inpt[0]) - 1
+        m = int(inpt)
     except ValueError:
         print(f">>    Error: {inpt} is not an int value")
         return promptM()
@@ -36,3 +36,26 @@ def promptM() -> int:
     return m
 
 
+def promptN(m: int) -> int:
+    print(f">>    Insert value for n")
+    inpt = input()
+    try:
+        n = int(inpt)
+    except ValueError:
+        print(f">>    Error: {inpt} is not an int value")
+        return promptN(m)
+    if n >= m:
+        print(f">>    Error: {n} >= {m} and n must be less than m")
+        return promptN(m)
+    return n
+
+
+def promptFloat() -> float:
+    print(f">>    Insert value for Y")
+    inpt = input()
+    try:
+        Y = float(inpt)
+    except ValueError:
+        print(f">>    Error: {inpt} is not a float value")
+        return promptFloat()
+    return Y
--- 3.  Inversions/main.py
+++ 3.  Inversions/main.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from functions import promptPair, promptM
+from functions import promptPair, promptM, promptN, promptFloat
 
 
 def F(x: float) -> float:
@@ -10,10 +10,10 @@ def F(x: float) -> float:
 def Lagrange(hublist: list):
 
     def Omega(x: float, xj: float):
-        return np.prod([xi - x for xi in hublist if xi != xj])
+        return np.prod([xi[0] - x for xi in hublist if xi[0] != xj])
 
     def func(x: float):
-        return sum([F(xj) * Omega(x, xj) / Omega(xj, xj) for xj in hublist])
+        return sum([xy[1] * Omega(x, xy[0]) / Omega(xy[0], xy[0]) for xy in hublist])
 
     return func
 
@@ -23,10 +23,29 @@ a, b = promptPair()
 m = promptM()
 
 h = (b - a) / m
-Xj = []
+ZYj = []
 for i in range(m+1):
-    Xj.append(a + i * h)
-Yj = [F(x) for x in Xj]
+    ZYj.append([a + i * h, F(a + i * h)])
+    print(f"F({ZYj[-1][0]}) = {ZYj[-1][1]}")
+
+n = promptN(m)
+
+Y = promptFloat()
+
+
+def cmp(val):
+    return abs(val[0] - Y)
+
+
+YXj = [[zy[1], zy[0]] for zy in ZYj]
+YXj.sort(key=cmp)
+YXj = YXj[:n + 1]
+
+L = Lagrange(YXj)
+X = L(Y)
+print("\nMethod 1:")
+print(f"L({Y}) = {X}")
+print(f"|F({X}) - {Y}| = {abs(F(X) - Y)}\n")
 
 
 
